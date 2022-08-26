52fdbeaa61efff0b107397634e7ee14e13e20f8a
samuel-wh <samuel@wisphub.net>
2022-08-26 17:15:58 -0500

    v1

--- apps/pokemon/urls.py
+++ apps/pokemon/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from views import  index, pokemon_list
+from views import  index, pokemon_details, pokemon_list
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^pokemon/listar$', pokemon_list, name='pokemon_listar'),
-
+    url(r'^pokemon/detalle/(?P<id_pokemon>\d+)/$', pokemon_details, name='pokemon_detalle'),
 ]
--- apps/pokemon/views.py
+++ apps/pokemon/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 import requests
 
@@ -23,7 +24,7 @@ def pokemon_list(request):
                 if response.status_code == 200:
                     payload = response.json()
                     sprites = payload.get('sprites',[])
-                    id_pokemon = payload['id']
+                    id_pokemon = payload.get('id',[])
                     sprite = sprites['front_default']
                     dir = {
                         'name':name, 
@@ -33,3 +34,39 @@ def pokemon_list(request):
                     pokemon_info.append(dir)
     return render(request,'pokemon/pokemon_listar.html',{'pokemon':pokemon_info})
 
+def pokemon_details(request, id_pokemon):
+    
+
+    url = "https://pokeapi.co/api/v2/pokemon/{}".format(id_pokemon)
+    response = requests.get(url)
+    if response.status_code == 200:
+        pokemon_details = []
+        dir1 = {}
+        payload = response.json()
+
+        sprites = payload.get('sprites',[])
+        sprite = sprites['front_default']
+        weight = payload['weight']
+        height = payload['height']
+        dir1 = {
+                'sprite':sprite,
+                'weight':weight,
+                'height':height,
+        }
+
+        """ types = payload.get('types',[])
+        if types:
+            type_list = []
+            dir_type = {}
+            for types in types:
+                type = types['type']
+
+        abilities = payload.get('abilities',[])
+        ability = abilities['ability']
+
+        moves = payload.get('moves',[])
+        move = moves['move'] """
+
+        
+        pokemon_details.append(dir1)
+    return render(request, 'pokemon/pokemon_detalles.html', {'detalle':pokemon_details})
